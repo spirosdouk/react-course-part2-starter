c2d5ab794d0b84e099efd8665606a23c435339e7
spirosdouk <s.doukeris@logicea.com>
2024-07-18 15:32:43 +0300

    Counter using zustand and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import PostList from "./react-query/PostList";
-import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/counter/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      <Counter />
+      <TaskList />
     </>
   );
 }
